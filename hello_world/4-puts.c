b2f4b0534f58fbf4d0d089f78cab8b3a629cd4cb
HectorPR4546 <h_perez93@yahoo.com>
2025-02-15 15:49:47 -0400

    Task # 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,7 @@
-#include <stdio.h> /* Main used to compile this function */
+#include <stdio.h>
 /* Task # 4, using puts command and Betty style */
-int main(void)
+
+int main(void) /* This is uses as stardart input */
 {
 	puts("\"Programming is like building a multilingual puzzle\n);
 	return (0);
