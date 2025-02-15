bf722e32a80817d519efddc5adf7b4c8f541f5b9
HectorPR4546 <h_perez93@yahoo.com>
2025-02-15 15:41:54 -0400

    Task # 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
+#include <stdio.h> /* Main used to compile this function */
 /* Task # 4, using puts command and Betty style */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle)\n;
+	puts("\"Programming is like building a multilingual puzzle\n);
 	return (0);
 }
