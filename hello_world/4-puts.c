7eece9fc01ffcfded97b100967bde4a0fe254032
HectorPR4546 <h_perez93@yahoo.com>
2025-02-15 15:38:20 -0400

    Task # 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,6 +2,6 @@
 /* Task # 4, using puts command and Betty style */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle)\n;
 	return (0);
 }
