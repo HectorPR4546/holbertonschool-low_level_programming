e4fc881f2b1e8320f9b1cc47b1981388038966fe
HectorPR4546 <h_perez93@yahoo.com>
2025-02-16 13:34:21 -0400

    Task # 0 and a README

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry poiny
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("%d \n, n);
+	if (n > 0)
+	{
+		printf("The number id positive \");
+	}
+	else if (n < 0)
+	{
+		printf("The number is negative \n");
+	}
+	else
+	{
+		printf("The number is zero \n");
+	return (0);
+}
---
+++ variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+In this rep i will add task abaut variables, if, else, while
