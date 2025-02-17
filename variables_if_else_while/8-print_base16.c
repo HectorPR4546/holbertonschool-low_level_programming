acf224b38f1d0b1a5fe662dc9e8c423729012aac
HectorPR4546 <h_perez93@yahoo.com>
2025-02-17 10:28:12 -0400

    Task # 8

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = '0'; a <= '15'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
