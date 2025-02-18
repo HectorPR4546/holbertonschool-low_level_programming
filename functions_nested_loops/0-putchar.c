cf39494228374edccc61bc4a5b52484e4f7cbfe8
HectorPR4546 <h_perez93@yahoo.com>
2025-02-18 18:33:57 -0400

    Task # 0 and a Readme

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,15 +1,20 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
- * main - entry point
- * Return: 0 (success)
+ * main -entry pint
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char *i = "_putchar\n";
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-	while (*i)
-	{
-		putchar(*i++);
-	}
 	return (0);
 }
