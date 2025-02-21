24a734fe7ca97c921a3d04764faf47af23100e57
HectorPR4546 <h_perez93@yahoo.com>
2025-02-21 13:39:41 -0400

    Task # 6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _abs - entry point
+ * @i: variabe
+ * Return: 0 (success)
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	return (-i);
+}
