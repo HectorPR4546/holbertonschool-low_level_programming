8226e1a1c066e58fdef1ac24fe0e7f5e811a529d
HectorPR4546 <h_perez93@yahoo.com>
2025-02-21 10:33:49 -0400

    Task # 4

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -6,14 +6,7 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
-	{
-		if (c >= 'A' && c <= 'Z')
-		{
-			return (1);
-		}
+	if (((c >= 'A') && (c <= 'Z')) || ((c >= 'a') && (c <= 'z')))
 		return (1);
-	}
-	else
-		return (0);
+	return (0);
 }
