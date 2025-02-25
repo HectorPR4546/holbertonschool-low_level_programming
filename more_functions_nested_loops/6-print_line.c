e3815b9f8fe258872be69cd09d01bde4d3235909
HectorPR4546 <h_perez93@yahoo.com>
2025-02-25 12:04:19 -0400

    Task # 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_line - entry point
+ * @n: variable
+ * Return: 0 (success)
+ */
+void print_line(int n)
+{
+while (n-- > 0)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
