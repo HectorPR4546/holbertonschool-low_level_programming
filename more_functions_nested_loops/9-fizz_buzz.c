3799908ef8aca17a98732ab4529017ffb16bb7f2
HectorPR4546 <h_perez93@yahoo.com>
2025-02-26 12:00:12 -0400

    Task # 9

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -3,20 +3,20 @@
  * main - entry point
  * Return: 0 (success)
  */
-int main()
+int main(void)
 {
 	int i;
 
-	for (i = 1; i<= 100; i++)
+	for (i = 1; i <= 100; i++)
 	{
-		if ( (i % 3 == 0) && (i % 5 == 0) )
+		if ((i % 3 == 0) && (i % 5 == 0))
 			printf("FizzBuzz");
 		else if (i % 3 == 0)
 			printf("Fizz");
 		else if  (i % 5 == 0)
 			printf("Buzz");
 		else
-			printf("%d",i);
+			printf("%d", i);
 		if (i < 100)
 			printf(" ");
 	}
