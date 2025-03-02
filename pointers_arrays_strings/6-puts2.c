fa369f9df759b8c4a0c0ccf138af3963cb9510fa
HectorPR4546 <h_perez93@yahoo.com>
2025-03-02 15:31:31 -0400

    Task # 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,18 @@
 #include "main.h"
 /**
- * puts2m - entry point
+ * puts2 - entry point
  * @str: value
  * Return 0 (success)
  */
+void puts2(char *str)
+{
+	int len;
+	int num;
 
+	for (len = 0; str[len] != '\0'; len++)
+		continue;
+
+	for (num = 0; num <= len - 1; num += 2)
+		_putchar(str[num]);
+	_putchar('\n');
+}
