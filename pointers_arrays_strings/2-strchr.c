16f60c0228d19affb86be0fe10ae2fb2ebcb1ae4
HectorPR4546 <h_perez93@yahoo.com>
2025-03-05 10:56:14 -0400

    Task # 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - entry point
+ * @s: variable
+ * @c: variable
+ * Return: 0 (success)
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		 if (*s == c)
+		 {
+			 return (s);
+		 }
+		 s++;
+	}
+	return (NULL);
+}
