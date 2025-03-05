467cfaca1133f6dfac32047520bed30fb8afbb4b
HectorPR4546 <h_perez93@yahoo.com>
2025-03-05 11:09:58 -0400

    Task # 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -10,17 +10,18 @@ char *_strchr(char *s, char c)
 {
 	while (*s != '\0')
 	{
-		 if (*s == c)
-		 {
-			 return (s);
-		 }
-		 else
-		 {
-			 s++;
-		 }
+		if (*s == c)
+		{
+			return (s);
+		}
+
+		else
+			s++;
 	}
+
 	if (*s == c)
 		return (s);
+
 	else
 		return (0);
 }
