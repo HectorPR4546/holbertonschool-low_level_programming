ae7eaf00ed1aaed27e108715181fbac459115587
HectorPR4546 <h_perez93@yahoo.com>
2025-03-06 17:44:35 -0400

    Task # 3

--- more_functions_nested_loops/3-main.c
+++ more_functions_nested_loops/3-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - check the code
@@ -7,6 +8,11 @@
  */
 int main(void)
 {
-    print_numbers();
+    char *s = "hello, world";
+    char *f = "oleh";
+    unsigned int n;
+
+    n = _strspn(s, f);
+    printf("%u\n", n);
     return (0);
 }
