a80ad3a45b412155a5ceefb14b64d3dd0c7e86a0
HectorPR4546 <h_perez93@yahoo.com>
2025-03-14 14:15:46 -0400

    Dog task # 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+#include <stdio.h>
+/**
+ * struct dog - the name of the structure
+ * @name: var
+ * @age: var
+ * @owner: var
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif /* _DOG_H_ */
