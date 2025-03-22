4c789ba1c5f9644f0d078dfd597804fa8f969b11
HectorPR4546 <h_perez93@yahoo.com>
2025-03-22 10:43:05 -0400

    Task # 3

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -5,4 +5,5 @@ void print_name(char *name, void (*f)(char *));
 int _putchar(char c);
 void array_iterator(int *arrayi, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+int op_add(int a, int b);
 #endif /* _FUNCTION_POINTERS_H_ */
