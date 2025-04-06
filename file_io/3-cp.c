eac6951384f4445d17c59b46d79880569669e9c7
HectorPR4546 <h_perez93@yahoo.com>
2025-04-06 13:34:31 -0400

    Task # 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -17,28 +17,28 @@
 
 int main(int argc, char **argv)
 {
-	int f, f2, filecheck;
+	int fd, fd2, filecheck;
 	char buffer[1024];
 
 	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	f = open(argv[1], O_RDONLY);
-	if (f == -1)
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	f2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (f2 == -1)
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
-	while ((filecheck = read(f2, buffer, 1024)) > 0)
+	while ((filecheck = read(fd, buffer, 1024)) > 0)
 	{
 		if (filecheck == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
-		filecheck = write(f2, buffer, filecheck);
+		filecheck = write(fd2, buffer, filecheck);
 		if (filecheck == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
@@ -50,10 +50,10 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(f) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f), exit(100);
-	if (close(f2) == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f2), exit(100);
+	if (close(fd) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd), exit(100);
+	if (close(fd2) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2), exit(100);
 
 	return (0);
 }
